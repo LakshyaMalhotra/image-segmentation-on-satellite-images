8d07820860205b83a4b1f49a5564039febd52c48
LakshyaMalhotra <lakshya9009@gmail.com>
2021-04-09 08:14:51 -0700

    Removed print statements.

--- unet.py
+++ unet.py
@@ -66,54 +66,43 @@ class UNet(nn.Module):
     def forward(self, x):
         # encoder
         x1 = self.down_block1(x)
-        print(x1.size())
         x1_down = self.max_pool(x1)
 
         x2 = self.down_block2(x1_down)
-        print(x2.size())
         x2_down = self.max_pool(x2)
 
         x3 = self.down_block3(x2_down)
-        print(x3.size())
         x3_down = self.max_pool(x3)
 
         x4 = self.down_block4(x3_down)
-        print(x4.size())
         x4_down = self.max_pool(x4)
 
         x5 = self.down_block5(x4_down)
-        print(x5.size())
 
         # decoder
         x_up = self.up_conv4(x5)
-        print(x_up.size())
         x4_connection = crop_connection(x4, x_up)
         x_up = self.up_block4(torch.cat([x4_connection, x_up], dim=1))
-        print(x_up.size())
 
         x_up = self.up_conv3(x_up)
-        print(x_up.size())
         x3_connection = crop_connection(x3, x_up)
         x_up = self.up_block3(torch.cat([x3_connection, x_up], dim=1))
-        print(x_up.size())
 
         x_up = self.up_conv2(x_up)
-        print(x_up.size())
         x2_connection = crop_connection(x2, x_up)
         x_up = self.up_block2(torch.cat([x2_connection, x_up], dim=1))
-        print(x_up.size())
 
         x_up = self.up_conv1(x_up)
-        print(x_up.size())
         x1_connection = crop_connection(x1, x_up)
         x_up = self.up_block1(torch.cat([x1_connection, x_up], dim=1))
-        print(x_up.size())
 
         x_up = self.conv_1x1(x_up)
-        print(x_up.size())
+
+        return x_up
 
 
 if __name__ == "__main__":
     image = torch.rand(1, 1, 572, 572)
     model = UNet()
     y = model(image)
+    print(y.size())
